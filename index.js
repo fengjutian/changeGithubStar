ab410fb8566d908e243a7a2a4901a9394183c54e
fengjutian <2640111081@qq.com>
2020-12-07 17:45:11 +0800

    save

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const Config = require('./file.js')
 const puppeteer = require('puppeteer');
 const readline = require('readline');
 const util = require('util');
-const readLine = util.promisify(readline);
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// const rlQuestion = util.promisify(rl.question);
 
 // 配置参数
 const login_url = 'https://github.com/login';
 const userName = Config.userName;
 const pwd = '';
 
-function readTerminal() {
+async function readTerminal() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
+  // rl.question('请输入密码：', (pwd) => {
+  //   console.log(2)
+  //   // TODO：将答案记录在数据库中。
+  //   console.log(`密码：${pwd}`);
+  // });
 
-  rl.question('请输入密码：', (pwd) => {
-    console.log(2)
-    // TODO：将答案记录在数据库中。
-    console.log(`密码：${pwd}`);
+  let result = await new Promise((resolve, reject) => {
+    rl.question('请输入密码：', (pwd) => {
+      console.log(2)
+      // TODO：将答案记录在数据库中。
+      resolve(pwd)
+      console.log(`密码：${pwd}`);
+    });
+  })
 
-  });
+  return result
 }
 
 
@@ -33,7 +47,8 @@ function readTerminal() {
   // 控制台读取密码
   console.log(1)
 
-  await readTerminal();
+  await readTerminal()
+
 
 
   console.log(3222)
