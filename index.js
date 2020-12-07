d53d672b8044e62352ebbb7958b7955165ec2490
fengjutian <2640111081@qq.com>
2020-12-07 17:23:21 +0800

    save

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-import { Login } from './file.js'
+const Config = require('./file.js')
 const puppeteer = require('puppeteer');
+const readline = require('readline');
+const util = require('util');
+const readLine = util.promisify(readline);
 
+// 配置参数
 const login_url = 'https://github.com/login';
-const userName = Login.userName;
-const pwd = Login.pwd
+const userName = Config.userName;
+const pwd = '';
+
+function readTerminal() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('请输入密码：', (pwd) => {
+    console.log(2)
+    // TODO：将答案记录在数据库中。
+    console.log(`密码：${pwd}`);
+
+  });
+}
+
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(login_url);
-    await page.type('#login_field', userName)
-    await page.type('#password', pwd)
-    await page.click('.session-authentication .auth-form .btn')
-    await page.waitForNavigation({
-        waitUntil: 'load'
-    })
-    await page.screenshot({path: 'githun.png'});
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // 登陆页面
+  await page.goto(login_url);
+  await page.type('#login_field', userName)
+  // 控制台读取密码
+  console.log(1)
 
-    await browser.close();
+  await readTerminal();
+
+
+  console.log(3222)
+
+  return false
+
+  await page.type('#password', pwd)
+  await page.click('.session-authentication .auth-form .btn')
+  // 跳转页面
+  await page.waitForNavigation({
+      waitUntil: 'load'
+  })
+  await page.screenshot({path: `./imgs/githun_${((new Date()).getTime())}.png`});
+
+  await browser.close();
+
+  rl.close();
+
 })();
