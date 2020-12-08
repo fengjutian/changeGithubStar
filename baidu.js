831bf60bd2ca3baf0204ad66c86bdec2fe203d75
fengjutian <2640111081@qq.com>
2020-12-08 15:20:01 +0800

    save

--- baidu.js
+++ baidu.js
@@ -1,11 +1,35 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: true, devtools: true});
   const page = await browser.newPage();
-  await page.goto('https://www.baidu.com/');
+  await page.goto('https://www.baidu.com/', { waitUntil: 'networkidle2' });
 
-  await page.screenshot({path: './imgs/baidu.png', fullPage:true});
+  // const dimensions = await page.evaluate(() => {
+  //   console.log("document.querySelector('#kw')", document.querySelector('#kw'))
+  //   return  document.querySelector('#kw')
+  //   // return {
+  //   //   width: document.documentElement.clientWidth,
+  //   //   height: document.documentElement.clientHeight,
+  //   //   deviceScaleFactor: window.devicePixelRatio
+  //   // };
+  // });
+
+  // console.log('Dimensions:', dimensions);
+
+
+  const inputEle = page.$$('#kw').then(res => {
+    console.log('res', res)
+    return res
+  });
+  console.log('inputEle', inputEle)
+
+
+
+
+  //await page.pdf({path: './pdf/baidu.pdf', format: 'A4'});
+
+  //await page.screenshot({path: './imgs/baidu.png', fullPage:true});
 
   await browser.close();
 })();
